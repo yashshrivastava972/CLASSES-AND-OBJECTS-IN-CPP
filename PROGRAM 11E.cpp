19e892f3bbf232bfa6941676c5d0196cd3be03cc
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-24 14:20:07 +0530

    Update PROGRAM 11E.cpp

--- PROGRAM 11E.cpp
+++ PROGRAM 11E.cpp
@@ -1,1 +1,32 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: USE OF PUBLIC & PRIVATE VARIABLES IN CLASS (CALCULATE VOLUME OF A CUBE WITH METHOD INSIDE CLASS & OUTSIDE CLASS) (EXPERIMENT: 11E)
 
+// CODE: 
+#include <iostream>
+using namespace std;
+
+class cube {
+    double height = 2.0;
+    double width = 3.0;
+    double length = 5.0;
+
+public:
+    double volume() {
+        double v;
+        v = height * width * length;
+        return v;
+    }
+};
+
+int main() {
+    cube cube1;
+    double vol = cube1.volume();
+    cout << "Volume: " << vol << endl;
+    return 0;
+}
+
+// OUTPUT:
+
+// Volume: 30
